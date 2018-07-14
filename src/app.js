e534a783b9dd2ac54e35c986a7ed8fdf6d5dcb7c
Tomás <tomas2387@gmail.com>
2018-07-14 20:47:31 +0200

    add video

--- src/app.js
+++ src/app.js
@@ -10,15 +10,58 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-	senseLeds.showMessage(':)', 0.5, [255, 0, 0]);
-	socket.on('chat message', (msg) => {
-		console.log('chat message received', msg)
-		senseLeds.showMessage(msg, 0.5, [133, 133, 133]);	
+    senseLeds.showMessage(':)', 0.2, [100, 100, 100]);
+    socket.on('chat message', (msg) => {
+        console.log('chat message received', msg)
+        senseLeds.showMessage(msg, 0.2, [133, 133, 133]);
         io.emit('chat message', msg);
     })
+    socket.on('image', (blob) => {
+        console.log('received image', blob)
+        var image = decodeBase64Image(blob)
+        new PNG().parse(image.data, convertPNG);
+    })
 });
+
+
+/**
+ * Decode base64 string to buffer.
+ *
+ * @param {String} base64Str string
+ * @return {Object} Image object with image type and data buffer.
+ * @public
+ */
+function decodeBase64Image(base64Str) {
+    var matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    var image = {};
+    if (!matches || matches.length !== 3) {
+        throw new Error('Invalid base64 string');
+    }
+
+    image.type = matches[1];
+    image.data = new Buffer(matches[2], 'base64');
+
+    return image;
+}
+
+
+function convertPNG(error, png) {
+    if (error) {
+        console.error(`Could not parse PNG ${error.message}`);
+        return;
+    }
+    console.log('parsed')
+    const pixels = pngTopixels(png);
+    console.log(pixels)
+    senseLeds.setPixels(pixels, callback);
+}
+
+function pngTopixels(png) {
+    return Array.from(new Array(png.width * png.height), (_, i) => {
+        return Array.from(new Array(3), (_, j) => png.data[i * 4 + j]);
+    });
+}
 
 http.listen(80, () => {
     console.log('listening on *:80');
 });
-
