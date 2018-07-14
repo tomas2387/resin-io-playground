3aa7db453002ce8515e0723234ee26303efad7b2
Tomás <tomas2387@gmail.com>
2018-07-14 17:42:09 +0200

    debugs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const senseLeds = require('sense-hat-led');
+
+console.log('conecting...')
 const socket = require('socket.io-client')('http://10.47.73.28:3000');
 
 socket.on('connect', () => {
+	console.log('connected!')
 	senseLeds.showMessage(':)', [255, 0, 0], () => {});
 })
 socket.on('chat message', (msg) => {
 	senseLeds.showMessage(msg, [255, 0, 0], () => {});
+})
+socket.on('disconect', () => {
+	console.log('disconnected :(')
+	senseLeds.showMessage(':)', [255, 0, 0], () => {});
 })
