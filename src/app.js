5a1f91fa53bed0cbb5cf24221d78a6809af6e8e3
Tomás <tomas2387@gmail.com>
2018-07-14 17:57:52 +0200

    try now

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 const senseLeds = require('sense-hat-led');
+const app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 console.log('conecting...')
-const socket = require('socket.io-client')('http://10.47.73.28:3000');
 
-socket.on('connect', () => {
-	console.log('connected!')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
 	senseLeds.showMessage(':)', [255, 0, 0], () => {});
-})
-socket.on('chat message', (msg) => {
-	senseLeds.showMessage(msg, [255, 0, 0], () => {});
-})
-socket.on('disconect', () => {
-	console.log('disconnected :(')
-	senseLeds.showMessage(':)', [255, 0, 0], () => {});
-})
+
+	socket.on('chat message', (msg) => {
+		console.log('chat message received', msg)
+		senseLeds.showMessage('msg', [255, 0, 0], () => {});	
+        io.emit('chat message', msg);
+    })
+});
+
+http.listen(80, () => {
+    console.log('listening on *:80');
+});
+
